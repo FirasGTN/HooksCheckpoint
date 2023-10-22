fd39165df6f3a9826d3d7fe23a36fc3fa8274b12
firas <firasazzebi66@gmail.com>
2023-10-22 01:11:33 +0100

    finalle project

--- src/components/MoviesDes.jsx
+++ src/components/MoviesDes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, useParams } from 'react-router-dom'
+import { Link, Routes, useParams } from 'react-router-dom'
 import {product} from '../products'
 import "./MoviesDesStyle.css"
 function MoviesDes() {
@@ -12,6 +12,9 @@ function MoviesDes() {
         <div className='des-item'>
           <h1>{obj.title}</h1>
           <h3>{obj.description}</h3>
+          <Link className='link-back' to={"/movies"}>
+            <button  className='bt-back' >Back</button>
+          </Link>
         </div>
         <iframe width="560" height="315" src= {obj.trailer} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
